d1448b2562d14d7f03ab308e6661a885912fdf03
Sàhíl Shàrmá <132856321+sahil-me@users.noreply.github.com>
2025-09-26 23:29:11 +0530

    Change output message
    Co-authored-by: Friend’s Name <thealgorithmiceye01@gmail.com>

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {      
         
-        System.out.println("Hello World !");
+        System.out.println("Yolo Badge!");
 
     }
 }
